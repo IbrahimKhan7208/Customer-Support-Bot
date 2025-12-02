7c6f73cac901497b084f116642477321e19c9a32
Ibrahim <khanibrahim7208@gmail.com>
2025-12-03 00:27:28 +0530

    Few commit

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,16 @@ import cors from "cors"
 const app = express()
 app.use(express.json());
 app.use(cors({
-  origin: "https://customer-support-bot-wxgu.onrender.com",
+  origin: [
+    "https://customer-support-bot-wxgu.onrender.com",
+    "https://customer-support-bot-backend-r3c4.onrender.com",
+    "http://localhost:5173"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.options("*", cors());
-
 const port = process.env.PORT
 
 app.get('/', (req, res)=>{
